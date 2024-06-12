5ddbdda217a4c21c7865b62c830812a15465e81f
BY-ee <gjwjqgh3@naver.com>
2024-06-12 22:17:13 +0900

    0612 2217

--- Test/Test.java
+++ Test/Test.java
@@ -3,8 +3,9 @@ public class Test {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String A = sc.next();
+        int re = sc.nextInt();
         
-        System.out.println(A);
+        System.out.println((A + " ").repeat(re));
 
     }
 }
