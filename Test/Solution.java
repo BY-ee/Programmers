0d8180cba7dc6da8e83aec396045ac2ed85d5bec
BY-ee <gjwjqgh3@naver.com>
2024-06-12 16:30:14 +0900

    Test 1628

---
+++ Test/Solution.java
@@ -0,0 +1,10 @@
+public class Solution {
+    public String solution(String my_string, int[] index_list) {
+        String answer = "";
+        
+        
+        while (index_list.length != result.length) {
+            if (String result : my_string[index_list]);
+        }
+    }
+}
