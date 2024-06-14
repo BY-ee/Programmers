55831536d01de2c00a02e78b60e20f8038a0575e
BY-ee <gjwjqgh3@naver.com>
2024-06-15 03:45:26 +0900

    24 0615 0344 little change

--- programmers/june/twenty/StringRepeat.java
+++ programmers/june/twenty/StringRepeat.java
@@ -9,9 +9,9 @@ import java.util.Scanner;
 public class StringRepeat {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String A = sc.next();
-        int n = sc.nextInt();
+        String A = sc.next();   // 반복할 문자열 입력
+        int n = sc.nextInt();   // 반복할 횟수 입력
         
-        System.out.println(A.repeat(n));
+        System.out.println(A.repeat(n));  // n번만큼 A 문자열을 반복 출력
     }
 }
