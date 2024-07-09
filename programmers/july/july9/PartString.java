198b8d7962fef8aa2d5c5b9a158422b283b1ac09
BY-ee <gjwjqgh3@naver.com>
2024-07-10 02:11:46 +0900

    2024 07.10 02:10 incomplete

--- programmers/july/july9/PartString.java
+++ programmers/july/july9/PartString.java
@@ -3,6 +3,10 @@
  *  https://school.programmers.co.kr/learn/courses/30/lessons/181842
  */
 
+/**
+ *  1. return str2.contains(str1) ? 1 : 0;   // 포함하는 문자열을 확인
+ *  2. return str2.indexOf(str1) > -1 ? 1 : 0;  // str1 문자열의 인덱스가 존재하면 인덱스 반환, 아니면 -1 반환
+ */
 
 package programmers.july.july9;
 
@@ -14,7 +18,8 @@ public class PartString {
             if(str1.equals(str2.substring(i - str1.length(), i))) answer = 1;
               // str2을 i - str1.length() 인덱스부터 i 인덱스까지 자른 문자열이 str1과 같으면 answer에 1 대입
         }
-
+        System.out.println("aa");
+            
         return answer;  // answer 반환
     }
 }
