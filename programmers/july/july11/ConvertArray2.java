8a462c722e3359f5ab1f9f41b1b9737ba1809807
BY-ee <gjwjqgh3@naver.com>
2024-07-11 23:51:40 +0900

    2024 07.11 23:50 solve

--- programmers/july/july11/ConvertArray2.java
+++ programmers/july/july11/ConvertArray2.java
@@ -3,31 +3,39 @@ import java.util.Arrays;
 
 public class ConvertArray2 {
     public int solution(int[] arr) {
-        int[] answer = arr.clone();
-
-        for(int i : answer) {
-            int idx = 0;
+        int[] arr2 = arr.clone();  // arr의 복제 배열 생성
+        int idx = 0;               // for-each문에 사용할 인덱스 변수 선언
+        int answer = 0;
+        
+        for(int i : arr2) {    // arr2의 값을 하나씩 추출
             if(i >= 50 && i % 2 == 0) {
-                answer[idx] = i / 2;
+                arr2[idx] = i / 2;
+                  // i가 50보다 크거나 같은 짝수라면 i / 2를 대입
             } else if(i < 50 && i % 2 == 1) {
-                answer[idx] = (i * 2) + 1;
+                arr2[idx] = (i * 2) + 1;
+                  // i가 50보다 작은 홀수라면 (i * 2) + 1을 대입
             }
+            
+            idx++;
         }
+        answer++;  // 배열의 계산이 끝난 후 answer에 + 1
 
-        while(!Arrays.equals(answer, arr)) {
-            arr = answer.clone();
+        while(!Arrays.equals(arr2, arr)) {  // arr과 arr2의 값이 같을 때까지 반복
+            idx = 0;
+            arr = arr2.clone();
 
-            for(int i : answer) {
-                int idx = 0;
+            for(int i : arr2) {
                 if(i >= 50 && i % 2 == 0) {
-                    answer[idx] = i / 2;
+                    arr2[idx] = i / 2;
                 } else if(i < 50 && i % 2 == 1) {
-                    answer[idx] = (i * 2) + 1;
+                    arr2[idx] = (i * 2) + 1;
                 }
+                
+                idx++;
             }
+            answer++;
         }
-        Arrays.sort(answer);
-
-        return answer[0];
+        
+        return answer - 1;  // answer에 - 1한 값을 대입
     }
 }
